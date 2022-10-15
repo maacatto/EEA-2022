83b07f0439ae5dfb0cd6bf1cbea3ed143b034b40
maacatto <mauricio.acatto@gmail.com>
2022-10-15 11:50:30 -0300

    algun mensaje de lo que voy a subir

--- actualizar.bat
+++ actualizar.bat
@@ -1,7 +1,32 @@
-REM #descartar los cambios que pudieran haber en las carpetas/archivos
-git checkout -f 
+rem @echo off
 
-REM #traer el repositorio a la maquina
+git add .
+if %errorlevel% NEQ 0 (goto END)  
+rem Agrego a staging todo lo que vengo trabajando y no subi a github aun. OJO, despues del add hay un espacio y un punto
+
+git commit -m "algun mensaje de lo que voy a subir"
+if %errorlevel% NEQ 0 (goto END)  
+
+git fetch upstream main 
+if %errorlevel% NEQ 0 (goto END)  
+
+rem Descarga cambios nuevos en el repo de los profes
+
+git merge upstream/main 
+if %errorlevel% NEQ 0 (goto END)  
+
+rem En este paso puede llegar a abrir un editor con un mensaje que dice que mezcla commits
+rem Alcanza con guardar el archivo y salir del editor
+
+git push 
+if %errorlevel% NEQ 0 (goto END)  
+
+rem Sube cambios propios con los nuevos del otro repo
+
+
 git pull 
+rem traer el repositorio a la maquina
+
+:END
 
 pause
